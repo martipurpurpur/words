ec6357d0d514f02193d002373ac4d04e54fd1b59
Margarita <marqari1a@gmail.com>
2019-08-16 23:06:13 +0300

    first lesson commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/rita', function () {
+    return view('rita');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
